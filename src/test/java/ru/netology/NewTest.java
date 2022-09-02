72792ed9a7d3e911687957284898f7bb27722e60
DariaSeliverstova <dashulia2004@yandex.ru>
2022-09-02 16:26:35 -0400

    added files

---
+++ src/test/java/ru/netology/NewTest.java
@@ -0,0 +1,63 @@
+package ru.netology;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class NewTest {
+
+    @Test
+    void shouldTransferMoneyFromFirstToSecond() {
+        open("http://localhost:9999");
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCode(authInfo);
+        var dashboardPage = verificationPage.validVerify(verificationCode);
+        int balanceFirstBeforeTransfer = dashboardPage.getCardBalance(0);
+        int balanceSecondBeforeTransfer = dashboardPage.getCardBalance(1);
+        var TransferMoneyPage = dashboardPage.cardRefill(0);
+        int amount = 2000;
+        TransferMoneyPage.transferManey (amount, DataHelper.getSecondCardInfo().getCardNumber());
+        Assertions.assertEquals(balanceFirstBeforeTransfer + amount, dashboardPage.getCardBalance(0));
+        Assertions.assertEquals(balanceSecondBeforeTransfer - amount, dashboardPage.getCardBalance(1));
+
+    }
+    @Test
+    void shouldTransferMoneyFromSecondToFirst() {
+        open("http://localhost:9999");
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCode(authInfo);
+        var dashboardPage = verificationPage.validVerify(verificationCode);
+        int balanceFirstBeforeTransfer = dashboardPage.getCardBalance(0);
+        int balanceSecondBeforeTransfer = dashboardPage.getCardBalance(1);
+        var TransferMoneyPage = dashboardPage.cardRefill(1);
+        int amount = 1000;
+        TransferMoneyPage.transferManey(amount, DataHelper.getFirstCardInfo().getCardNumber());
+        Assertions.assertEquals(balanceFirstBeforeTransfer - amount, dashboardPage.getCardBalance(0));
+        Assertions.assertEquals(balanceSecondBeforeTransfer + amount, dashboardPage.getCardBalance(1));
+    }
+
+    @Test
+    void shouldOverLimit() {
+        open("http://localhost:9999");
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCode(authInfo);
+        var dashboardPage = verificationPage.validVerify(verificationCode);
+        int balanceFirstBeforeTransfer = dashboardPage.getCardBalance(0);
+        int balanceSecondBeforeTransfer = dashboardPage.getCardBalance(1);
+        var TransferMoneyPage = dashboardPage.cardRefill(0);
+        int amount = 10000;
+        TransferMoneyPage.transferManey (amount, DataHelper.getSecondCardInfo().getCardNumber());
+        Assertions.assertEquals(balanceFirstBeforeTransfer + amount, dashboardPage.getCardBalance(0));
+        Assertions.assertEquals(balanceSecondBeforeTransfer - amount, dashboardPage.getCardBalance(1));
+
+    }
+}
